ea7791895d6a5b445fd50d279ac8780ad2bf1686
Abe Suni M. Caymo <45987443+abetoots@users.noreply.github.com>
2020-01-23 19:55:45 +0800

    separate header into component, update footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,75 +1,41 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import "../styles/styles.scss";
 
-import { rhythm, scale } from "../utils/typography"
+import { rhythm } from "../utils/typography";
+import Header from "./Header/Header";
 
-class Layout extends React.Component {
-  render() {
-    const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    let header
-
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
+const Layout = props => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
     }
-    return (
-      <div
+  `);
+  const { children } = props;
+  const rootPath = `${__PATH_PREFIX__}/`;
+  console.log(rootPath);
+
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <main style={{ padding: `${rhythm(1 / 2)} ${rhythm(1)}` }}>
+        {children}
+      </main>
+      <footer
         style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          color: "white",
+          backgroundColor: "black",
+          padding: `${rhythm(1 / 2)}`,
         }}
       >
-        <header>{header}</header>
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    )
-  }
-}
+        © writtentrove.com {new Date().getFullYear()}
+      </footer>
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
