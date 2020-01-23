5501a2e7842e2553cd1d7205d83989f7159042b4
Abe Suni M. Caymo <45987443+abetoots@users.noreply.github.com>
2020-01-23 19:58:33 +0800

    override font family

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,23 +1,26 @@
-import Typography from "typography"
-import Wordpress2016 from "typography-theme-wordpress-2016"
+import Typography from "typography";
+import Wordpress2016 from "typography-theme-wordpress-2016";
 
 Wordpress2016.overrideThemeStyles = () => {
   return {
     "a.gatsby-resp-image-link": {
       boxShadow: `none`,
     },
-  }
-}
+  };
+};
+//no need, import google fonts/npm typefaces through gatsby-browser
+delete Wordpress2016.googleFonts;
+//lets override the font families, dont forget to add their typefaces in gatsby-browser
+Wordpress2016.headerFontFamily = ["Montserrat", "Helvetica", "sans-serif"];
+Wordpress2016.bodyFontFamily = ["Open Sans", "Helvetica", "sans-serif"];
 
-delete Wordpress2016.googleFonts
-
-const typography = new Typography(Wordpress2016)
+const typography = new Typography(Wordpress2016);
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
-  typography.injectStyles()
+  typography.injectStyles();
 }
 
-export default typography
-export const rhythm = typography.rhythm
-export const scale = typography.scale
+export default typography;
+export const rhythm = typography.rhythm;
+export const scale = typography.scale;
