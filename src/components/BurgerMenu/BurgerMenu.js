fff76c2fee41e57be337e592a2be94e90a665828
Abe Suni M. Caymo <45987443+abetoots@users.noreply.github.com>
2020-06-11 06:27:03 +0800

    update formatting

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -1,30 +1,26 @@
-import React from 'react';
-import './BurgerMenu.scss';
+import React from "react";
+import "./BurgerMenu.scss";
 
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-
-const BurgerMenu = (props) => {
-    const burgerClasses = ['BurgerMenu'];
-
-    if (props.toggled) {
-        burgerClasses.push('-toggled');
-    }
-
-    return (
-        <button className={burgerClasses.join(' ')} onClick={props.handleClick}>
-            <div className="BurgerMenu__bar -one"></div>
-            <div className="BurgerMenu__bar -two"></div>
-            <div className="BurgerMenu__bar -three"></div>
-            <span className="BurgerMenu__label">Menu</span>
-        </button>
-    );
-}
+const BurgerMenu = props => (
+  <button
+    className={`BurgerMenu ${props.toggled ? "-toggled" : ""}`}
+    aria-expanded={props.toggled}
+    onClick={props.handleClick}
+  >
+    <div className="BurgerMenu__bar -one"></div>
+    <div className="BurgerMenu__bar -two"></div>
+    <div className="BurgerMenu__bar -three"></div>
+    <span className={`BurgerMenu__label ${props.hideLabel ? "-hidden" : ""}`}>
+      Menu
+    </span>
+  </button>
+);
 
 BurgerMenu.propTypes = {
-    handleClick: PropTypes.func.isRequired,
-    toggled: PropTypes.bool
-}
-
+  handleClick: PropTypes.func.isRequired,
+  toggled: PropTypes.bool,
+};
 
 export default BurgerMenu;
